beca3d4d7ce7a71750a5b5e914b5a3b1d470a380
Nguyen Khac Nhat <khacnhat.info@gmail.com>
2019-03-17 18:10:40 +0700

    .

--- sh/travis.sh
+++ sh/travis.sh
@@ -5,5 +5,5 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   ./pipe_build_up_test.sh
   curl -O https://raw.githubusercontent.com/cyber-dojo/cyber-dojo/master/shared/push_and_trigger.sh
   chmod +x push_and_trigger.sh
-  ./push_and_trigger.sh cyber-dojo/web
+  ./push_and_trigger.sh khacnhat/web
 fi
